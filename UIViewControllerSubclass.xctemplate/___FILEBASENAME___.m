59dea6399ee0100738a4e2d99edb29290af051b8
Bernhard Loibl <bernhard.loibl@googlemail.com>
2012-09-12 10:28:49 +0200

    customized viewcontroller template

--- UIViewControllerSubclass.xctemplate/___FILEBASENAME___.m
+++ UIViewControllerSubclass.xctemplate/___FILEBASENAME___.m
@@ -1,50 +1,69 @@
 #import "___FILEBASENAME___.h"
+#import "DDLog.h"
+
+// Log-Levels: off, error, warn, info, verbose
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 
 @interface ___FILEBASENAMEASIDENTIFIER___ ()
-- (void)releaseViews;
+
 @end
 
 @implementation ___FILEBASENAMEASIDENTIFIER___
 
-//@synthesize var = var_;
-
-- (void)dealloc
-{
-  //RELEASE(propertyName);
-  
-  [self releaseViews];
-  [super dealloc];
-}
-
-- (void)releaseViews
-{
-  
-}
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-  if((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]))
-  {
+  if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
       
   }
   
   return self;
 }
 
-#pragma mark - UIViewController
+#pragma mark -
+#pragma mark <#Label#>
 
 - (void)loadView
 {
-  [super loadView];
   //UIView *view = [self view];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    
+    [super viewDidDisappear:animated];
 }
 
 - (void)viewDidUnload
 {
-  [self releaseViews];
+
   [super viewDidUnload];
 }
 
-#pragma mark - Other methods
+#pragma mark -
+#pragma mark <#Label#>
 
 @end
